f4c0e12c43da5e31c90bc4f72b7b9d7831604dde
Danilo Delizia <ddelizia@gmail.com>
2025-12-10 13:07:33 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
-  integrations: [react()],
+  integrations: [react(), mdx()],
+  adapter: cloudflare(),
 });
